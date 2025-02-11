72ccc1fe864fcbf7928268abb86711c804d6a254
David Gomez <davidgd2992@gmail.com>
2025-02-11 15:06:50 -0500

    Implement Husky, Improve: index, manifest, eslint config, remove unnecesary dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { visualizer } from "rollup-plugin-visualizer";
-import compression from "vite-plugin-compression";
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: 'react',
       babel: {
         plugins: [
-          ["@babel/plugin-transform-react-jsx", { optimize: true }],
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
         ],
       },
     }),
-    splitVendorChunkPlugin(),
     compression({
-      algorithm: "gzip",
-      ext: ".gz",
+      algorithm: 'gzip',
+      ext: '.gz',
     }),
     compression({
-      algorithm: "brotliCompress",
-      ext: ".br",
+      algorithm: 'brotliCompress',
+      ext: '.br',
     }),
     visualizer({
-      filename: "./dist/stats.html",
+      filename: './dist/stats.html',
       gzipSize: true,
       brotliSize: true,
     }),
@@ -38,36 +39,36 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@types": path.resolve(__dirname, "./src/types"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@constants": path.resolve(__dirname, "./src/constants"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
   optimizeDeps: {
     include: [
-      "react",
-      "react-dom",
-      "react-router-dom",
-      "framer-motion",
-      "immer",
-      "axios",
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'framer-motion',
+      'immer',
+      'axios',
     ],
-    exclude: ["@vitejs/plugin-react"],
+    exclude: ['@vitejs/plugin-react'],
     esbuildOptions: {
-      target: "esnext",
+      target: 'esnext',
       treeShaking: true,
     },
   },
   build: {
-    target: "esnext",
-    sourcemap: process.env.NODE_ENV === "development",
-    minify: "esbuild",
+    target: 'esnext',
+    sourcemap: process.env.NODE_ENV === 'development',
+    minify: 'esbuild',
     cssCodeSplit: true,
     assetsInlineLimit: 4096,
     chunkSizeWarningLimit: 1000,
@@ -75,50 +76,41 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: [
-            "react",
-            "react-dom",
-            "react-router-dom",
-          ],
+          vendor: ['react', 'react-dom', 'react-router-dom'],
           ui: [
-            "@heroui/button",
-            "@heroui/input",
-            "@heroui/react",
-            "@heroui/spinner",
-            "@heroui/system",
-            "@heroui/theme",
+            '@heroui/button',
+            '@heroui/input',
+            '@heroui/react',
+            '@heroui/spinner',
+            '@heroui/system',
+            '@heroui/theme',
           ],
-          utils: [
-            "axios",
-            "immer",
-            "use-immer",
-            "framer-motion",
-          ],
+          utils: ['axios', 'immer', 'use-immer', 'framer-motion'],
         },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split(".");
+        assetFileNames: assetInfo => {
+          const info = assetInfo.name.split('.');
           let extType = info[info.length - 1];
           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
+            extType = 'img';
           } else if (/woff|woff2/.test(extType)) {
-            extType = "fonts";
+            extType = 'fonts';
           }
           return `assets/${extType}/[name]-[hash][extname]`;
         },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       },
     },
   },
   define: {
-    "process.env": process.env,
+    'process.env': process.env,
   },
-  envDir: "./",
+  envDir: './',
   preview: {
     port: 3000,
     strictPort: true,
     headers: {
-      "Cache-Control": "public, max-age=31536000",
+      'Cache-Control': 'public, max-age=31536000',
     },
   },
 });
